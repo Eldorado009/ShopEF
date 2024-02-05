7f36daa45bd1d47c5a9ac9efd1565c5b178b1785
Eldorado009 <eldarna@code.edu.az>
2024-02-05 05:38:28 +0400

    Interface Users Added

---
+++ Shop.Business/Interface/IUser.cs
@@ -0,0 +1,14 @@
+﻿using Shop.Core.Entities;
+
+namespace Shop.Business.Interface;
+
+public interface IUser
+{
+    void CreateUser();
+    void DeleteUser();
+    void ActivateProfile();
+    void DeActivateProfile();
+    void Login();
+    void UpdateProfile();
+    Task <List<User>> GetAllUsers();
+}
