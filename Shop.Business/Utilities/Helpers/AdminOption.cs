46562d822053facd2fc1f26b7446702ab72d9b2c
Eldorado009 <eldarna@code.edu.az>
2024-02-08 07:44:26 +0400

    Added Helpers

---
+++ Shop.Business/Utilities/Helpers/AdminOption.cs
@@ -0,0 +1,20 @@
+﻿namespace Shop.Business.Utilities.Helpers;
+
+public enum AdminOption
+{
+    CreateUser = 1,
+    UpdateProfile,
+    DeleteUser,
+    GetAllUsers,
+    ActivateProfile,
+    DeactivateProfile,
+    CreateProduct,
+    UpdateProduct,
+    DeleteProduct,
+    ActivateProduct,
+    DeactivateProduct,
+    GetAllProducts,
+    CreateBrand,
+    CreateCategory,
+    Exit
+}
---
+++ Shop.Business/Utilities/Helpers/UserOption.cs
@@ -0,0 +1,21 @@
+﻿namespace Shop.Business.Utilities.Helpers;
+
+public enum UserOption
+{
+    GetAllProducts =1,
+    GetAllCards,
+    CreateCard,
+    UpdateCard,
+    DeleteCard,
+    CardExists,
+    GetCardBalance,
+    GetAllWallets,
+    CreateWallet,
+    UpdateWallet,
+    DeleteWallet,
+    GetWalletBalance,
+    IncreaseWalletBalance,
+    CreateProductInvoice,
+    CreateInvoice,
+    Exist
+}
